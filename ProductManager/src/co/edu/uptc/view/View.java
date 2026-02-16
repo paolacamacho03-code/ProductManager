9530b25b1f581796b04209e6ff850f598b29d032
Paola044 <paolacamacho044@gmail.com>
2026-02-16 17:14:36 -0500

    Se creó el "esqueleto" del proyecto

---
+++ ProductManager/src/co/edu/uptc/view/View.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.uptc.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paola
+ */
+public class View {
+    
+    private Scanner input;
+
+    public View() {
+        input = new Scanner(System.in);
+    }
+
+}
